8930590539a8fa157324542e14f3579e4742541a
Marcos <marcosvr.lima96@gmail.com>
2021-12-08 14:17:58 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/exp_package_datatable2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exp_package_datatable2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: DataTable2SimpleDemo(),
+    );
+  }
+}
+
+/// Example without a datasource
+class DataTable2SimpleDemo extends StatelessWidget {
+  const DataTable2SimpleDemo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: DataTable2(
+            columnSpacing: 12,
+            horizontalMargin: 12,
+            minWidth: 600,
+            columns: [
+              DataColumn2(
+                label: Text('Column A'),
+                size: ColumnSize.L,
+              ),
+              DataColumn(
+                label: Text('Column B'),
+              ),
+              DataColumn(
+                label: Text('Column C'),
+              ),
+              DataColumn(
+                label: Text('Column D'),
+              ),
+              DataColumn(
+                label: Text('Column NUMBERS'),
+                numeric: true,
+              ),
+            ],
+            rows: List<DataRow>.generate(
+                100,
+                (index) => DataRow(cells: [
+                      DataCell(Text('A' * (10 - index % 10))),
+                      DataCell(Text('B' * (10 - (index + 5) % 10))),
+                      DataCell(Text('C' * (15 - (index + 5) % 10))),
+                      DataCell(Text('D' * (15 - (index + 10) % 10))),
+                      DataCell(Text(((index + 0.1) * 25.4).toString()))
+                    ]))),
+      ),
+    );
+  }
+}
